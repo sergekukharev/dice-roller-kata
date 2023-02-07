198036b600b463c52685ca5d212f2cd9fc906700
Sergii Kukhariev <serge.kukharev@gmail.com>
2023-02-07 13:42:04 -0300

    Hello, world!

---
+++ src/DiceRoller.test.ts
@@ -0,0 +1,7 @@
+import DiceRoller from "./DiceRoller";
+
+describe('Dice roller', () => {
+    it('can greet', () => {
+        new DiceRoller().helloWorld();
+    })
+})
---
+++ src/DiceRoller.ts
@@ -0,0 +1,5 @@
+export default class DiceRoller {
+    helloWorld(): string {
+        return "Hello, world";
+    }
+}
